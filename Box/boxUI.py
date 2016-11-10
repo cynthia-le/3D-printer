6c7ac83a4474100176481b97f222bb6067a99d61
maxwell <maxwellabrams1@gmail.com>
2016-11-10 09:16:06 -0800

    Add "Hello World"
    -Create folder for Box UI resources
    -Show Tkinter usage

---
+++ Box/boxUI.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#More info at https://www.tutorialspoint.com/python/python_gui_programming.htm
+
+import Tkinter
+top = Tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
